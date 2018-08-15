30ac6830b217f623a349a0e304a1ab64c765020d
余谦 <yuqian@ipaynow.cn>
2018-08-15 18:43:44 +0800

    新建简单springMVC项目

---
+++ src/main/java/com/yuqian/ssmvc/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.yuqian.ssmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+   @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String index() {
+        System.out.println("进入首页");
+        return "index";
+    }
+}
